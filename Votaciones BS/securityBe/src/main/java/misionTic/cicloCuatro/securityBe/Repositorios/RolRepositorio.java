13d525fa1ad807e649a9f26f9cb00626da581a96
Sergio2709 <111620478+Sergio2709@users.noreply.github.com>
2022-11-21 20:15:55 -0500

    Delete Votaciones BS/securityBe directory

--- Votaciones BS/securityBe/src/main/java/misionTic/cicloCuatro/securityBe/Repositorios/RolRepositorio.java
+++
@@ -1,8 +0,0 @@
-package misionTic.cicloCuatro.securityBe.Repositorios;
-
-import misionTic.cicloCuatro.securityBe.Modelos.Rol;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-public interface RolRepositorio extends MongoRepository<Rol,String> {
-}
